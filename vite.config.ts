8042db7137f913dd0b3be81af4e08a2161e36dad
p3rrg0d <arturkoterlin@gmail.com>
2025-12-23 14:09:24 +0200

    feat: add vite configuration for project base path and build output

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/monobank-customizer/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+    }
+});
